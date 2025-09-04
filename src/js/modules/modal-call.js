a06d76fd5110e0a9f4729003cac225b6efb60ce8
unknown <jarulcrip@gmail.com>
2025-09-04 20:03:42 +0300

    ready all adaptive

---
+++ src/js/modules/modal-call.js
@@ -0,0 +1,32 @@
+const dialog = document.querySelector('.dialog');
+const openModal = document.querySelector('.modal-call');
+const closeModal = dialog.querySelector('.close-btn');
+
+function openModalBlockScroll() {
+	dialog.showModal();
+	document.body.classList.add('no-scroll');
+}
+
+function returnScroll() {
+	document.body.classList.remove('no-scroll');
+}
+
+function close() {
+	dialog.close();
+	returnScroll();
+}
+
+openModal.addEventListener('click', openModalBlockScroll);
+closeModal.addEventListener('click', close);
+
+function closeOverlayClick({ currentTarget, target }) {
+	const dialog = currentTarget;
+	const isOnOverlayClick = target === dialog;
+	if (isOnOverlayClick) {
+		close();
+	}
+}
+dialog.addEventListener('click', closeOverlayClick);
+dialog.addEventListener('cancel', returnScroll);
+
+export default modal;
